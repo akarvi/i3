556c32980e9b5c1f19710cc9688898c43b663911
Alex <nesingho@datendezernat.de>
2018-06-29 22:05:57 +0200

    delete obsolete files

--- usr/lib/i3blocks/date.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env sh
-#date script, put it in your i3blocks scripts folder, here /usr/lib/i3blocks
-case $BLOCK_BUTTON in
-	1)orage ;;
-	2)xclock ;;
-	3)xcalc ;;
-esac
-date '+%A %e. %B   %H:%M   '
